6653ae6b2a5a8a71727e414433910f7ff874efaa
AMAN JAISWAL <124071449+amanjaiswal7236@users.noreply.github.com>
2024-02-11 01:16:15 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Notion } from './components/Notion'
+
+
+function App() {
+  
+  return (
+    <>
+      <Notion />
+    </>
+  )
+}
+
+export default App
